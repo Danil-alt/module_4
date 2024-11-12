e4b010f385649550a62761c2d63093d913ed0817
Danil-alt <kilindany24@gmail.com>
2024-11-12 10:46:04 +0500

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,10 @@
+def test_function():
+    def inner_function():
+        return print('Я в области видимости функции test_function')
+
+    inner_function()
+    return
+
+
+test_function()
+inner_function()
