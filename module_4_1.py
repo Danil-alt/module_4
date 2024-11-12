251d6bfe63b24ae450a91d08edcf7542b0143565
Danil-alt <kilindany24@gmail.com>
2024-11-12 11:51:23 +0500

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,7 +1,11 @@
 import fake_math as fm
 import true_math as tm
 
-fm.divide(13, 1)
-fm.divide(13, 0)
-tm.divide(13, 1)
-tm.divide(13, 0)
+result1 = fm.divide(13, 1)
+result2 = fm.divide(13, 0)
+result3 = tm.divide(13, 1)
+result4 = tm.divide(13, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
